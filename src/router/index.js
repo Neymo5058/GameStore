ac847ad9f2ba0c54437cafec96a2e40635f3e761
Neymo5058 <vadam7480@gmail.com>
2025-07-26 22:32:46 -0400

    Add admin routes to router configuration

--- src/router/index.js
+++ src/router/index.js
@@ -6,8 +6,11 @@ const routes = [
   { path: "/signup", name: "SignUp", component: SignUpPage },
   { path: "/game", name: "Game", component: Game },
   { path: "/login", name: "SignUp", component: SignUpPage },
+  { path: "/admin/users", name: "", component: SignUpPage },
+  { path: "/admin/users/add", name: "", component: SignUpPage },
+  { path: "/admin/items", name: "", component: SignUpPage },
+  { path: "/admin/items/add", name: "", component: SignUpPage },
 ];
-
 const router = createRouter({
   history: createWebHistory(),
   routes,
