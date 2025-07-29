a13d3d609cf45fdcf7ed59e2a6b9337e19d94cb3
Neymo5058 <vadam7480@gmail.com>
2025-07-29 18:57:00 -0400

    Remove

--- server/routes/gameRoutes.js
+++ server/routes/gameRoutes.js
@@ -5,8 +5,5 @@ const router = express.Router();
 
 router.post("/", GameController.create);
 router.get("/:gameId", GameController.getItem);
-router.get("/test", (req, res) => {
-  res.send("Game test route OK");
-});
 
 export default router;
